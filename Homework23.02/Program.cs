8c585dced722933686822569367c5054b4b61007
mariam <mariam@MYPROBOOK>
2023-02-23 19:10:06 +0400

    Add project files.

---
+++ Homework23.02/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Homework23._02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter dirty salary: ");
+            double dirtySalary = double.Parse(Console.ReadLine());
+
+            double cleanSalary;
+            CalculateCleanSalary(dirtySalary, out cleanSalary);
+
+            Console.WriteLine("Clean salary: " + cleanSalary);
+        }
+
+        static void CalculateCleanSalary(double dirtySalary, out double cleanSalary)
+        {
+            double militaryFee = 0.1 * dirtySalary;
+            double taxes = 0.05 * dirtySalary;
+
+            cleanSalary = dirtySalary - militaryFee - taxes;
+        }
+    }
+}
